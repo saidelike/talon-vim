8aa53d37ac9d0578a3028f9d683e5282403e3443
Cedric Halbronn <cedric.halbronn@nccgroup.com>
2024-02-16 15:38:57 +0000

    also match the vim app

--- terminal/term_apps.py
+++ terminal/term_apps.py
@@ -3,14 +3,17 @@ from talon import Module
 mod = Module()
 
 mod.apps.cmd = r"""
+app: vim
 win.title: /TERM:C:\\Windows\\system32\\cmd.exe/
 """
 
 mod.apps.repl = r"""
+app: vim
 win.title: /TERM:Talon - REPL/
 """
 
 mod.apps.git_bash = r"""
+app: vim
 win.title: /TERM:MINGW64/
 """
 
@@ -19,5 +22,6 @@ win.title: /TERM:MINGW64/
 # xterm*|rxvt*)
 #   PS1="\[\e]0;${debian_chroot:+($debian_chroot)}wsl: \w\a\]$PS1"
 mod.apps.wsl = r"""
+app: vim
 win.title: /TERM:wsl/
 """
